e79dd4f3c3a23c0ab9836da9c25269af8643f46e
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-05-08 11:39:02 +0600

    initial project setup

---
+++ Test Project/Shared/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Shared
+//
+//  Created by Guest User on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
